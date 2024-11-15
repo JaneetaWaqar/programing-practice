55ae335b4d2172ce58f67402057f5ad44570d0fb
Janeeta Waqar <waqarjaneeta@gmail.com>
2024-11-15 05:34:56 -0800

    Add files via upload

---
+++ if_else program.cpp
@@ -0,0 +1,19 @@
+# include<iostream>
+using namespace std;
+int main()
+{
+	int percentage;
+	
+	cout<<"please enter your percentage";
+	cin>>percentage;
+	
+	if(percentage<=50)
+	 {
+		cout<<"percentage less than 50 become fail";
+	 }
+	 
+	else(percentage>50);
+	   {
+		cout<<"percentage greater than 50 get grade D";
+	   }	
+}
