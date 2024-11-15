723f5ad954ca68e44000b235ecb6bc22885cbb24
Janeeta Waqar <waqarjaneeta@gmail.com>
2024-11-15 05:45:07 -0800

    Add files via upload

---
+++ if_program.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+	int valueone=10;
+	int valuetwo=20;
+	int valuethree=30;
+	cout<<"Hello world"<<endl;
+	cout<<"Hi"<<endl;
+	if ( valueone>valuetwo )
+	{cout<<"this is the value";
+	if ( valuethree<valuetwo )
+		cout<<"Hy pakistan";
+	}
+	cout<<"this is not in if"<<endl;
+	
+}
